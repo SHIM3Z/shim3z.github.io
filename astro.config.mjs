037bdef86b35dcc883ddd3bd97e8ff0bff75c1c7
SHIM3Z <shim3z151002@gmail.com>
2025-12-27 12:20:37 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shim3z.github.io",
-  base: "shim3z.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://shim3z.github.io",
+  base: "shim3z.github.io",
 });
